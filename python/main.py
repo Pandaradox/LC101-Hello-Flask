a66a7f7b9a0bf9aed2f1be2aa70e05a956d40da2
Pandaradox <jacardarella@gmail.com>
2018-10-15 11:20:38 -0500

    week10 init

--- python/main.py
+++ python/main.py
@@ -1,17 +1,12 @@
-# ########################################
-def main():
-    from flask import Flask
+from flask import Flask
 
-    app = Flask(__name__)
-    app.config['DEBUG'] = True
+app = Flask(__name__)
+app.config['DEBUG'] = True
+form = """
+"""
 
-    @app.route("/")
-    def index():
-        return("Hello World")
+@app.route("/")
+def index():
+    return ("Hello World")
 
-
-    app.run()
-
-
-if __name__ == "__main__":
-    main()
+app.run()
